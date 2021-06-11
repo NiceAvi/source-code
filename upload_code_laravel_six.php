4295706dcbc327e5772ff1c5328ff3e233e34c4f
NiceAvi <niceavi007@gmail.com>
2021-06-11 22:10:18 +0530

    Upload code in Laravel 6
    insert and update code

---
+++ upload_code_laravel_six.php
@@ -0,0 +1,25 @@
+$documents = LeadDocuments::where('lead_id', $request->id)->get();
+        if ($documents != null) {
+            foreach ($documents as $document) {
+                $image_path = public_path() . '/files/' . $document->file;
+
+                if (File::exists($image_path)) {
+                    File::delete($image_path);
+                }
+            }
+            $documents = LeadDocuments::where('lead_id', $request->id)->delete();
+        }
+        if ($request->hasFile('file')) {
+            //
+            $i = 0;
+            foreach ($request->file('file') as $file) {
+                $fileName = time() . rand(0, 1000) . pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                $fileName = $fileName . '.' . $file->getClientOriginalExtension();
+                $file->move(public_path() . '/files/', $fileName);
+                $input['doc_id'] = $request->doc_id[$i];
+                $input['lead_id'] = $lead->id;
+                $input['file'] = $fileName;
+                LeadDocuments::create($input);
+                $i++;
+            }
+}
