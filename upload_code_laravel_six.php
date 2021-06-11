635bc3b5cfcc133b30f71bb32ffe343cf5812fdc
NiceAvi <niceavi007@gmail.com>
2021-06-11 22:11:43 +0530

    insert and update code

--- upload_code_laravel_six.php
+++ upload_code_laravel_six.php
@@ -1,3 +1,21 @@
+// this code is for insert upload file
+if ($request->hasFile('file')) {
+            //
+            $i = 0;
+            foreach ($request->file('file') as $file) {
+                $fileName = time() . rand(0, 1000) . pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                $fileName = $fileName . '.' . $file->getClientOriginalExtension();
+                $file->move(public_path() . '/files/', $fileName);
+                $input['doc_id'] = $request->doc_id[$i];
+                $input['lead_id'] = $lead->id;
+                $input['file'] = $fileName;
+                LeadDocuments::create($input);
+                $i++;
+            }
+        }
+
+// this code is for update upload file
+
 $documents = LeadDocuments::where('lead_id', $request->id)->get();
         if ($documents != null) {
             foreach ($documents as $document) {
